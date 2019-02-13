aa3200289062016bd056c888d7a5429e2eddb363
lingxiao <FangMajesty@163.com>
2019-02-13 14:27:17 +0800

    使用feign更新工程

--- src/main/java/com/springcloud/dream/mall/MallApplication.java
+++ src/main/java/com/springcloud/dream/mall/MallApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
@@ -12,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @MapperScan(value = "com.springcloud.dream.mall.dao")
 @EnableDiscoveryClient
+@EnableFeignClients
 public class MallApplication {
     @Bean
     public RestTemplate restTemplate() {
